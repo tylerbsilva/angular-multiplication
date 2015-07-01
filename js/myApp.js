2c14ca3adf18cebee4789bcefb684f6cb3b42ee0
Tyler Silva <tylerbsilva@gmail.com>
2015-07-01 13:50:49 -0500

    Added user input

--- js/myApp.js
+++ js/myApp.js
@@ -1,5 +1,15 @@
 var app = angular.module('myApp', []);
 
-app.controller('MultiplicationCtrl', function($scope) {
-  $scope.numbers = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+app.controller('MultiplicationCtrl', function($scope, $attrs) {
+  $scope.numberLimit = $attrs.initialNumberLimit || 10;
+  $scope.numbers = function() {
+    var numbers = [];
+    for(var i=0; i<$scope.numberLimit; i++) {
+      numbers[i] = i + 1;
+    }
+    return numbers;
+  };
+  $scope.compute = function(a,b) {
+    return a * b;
+  };
 });
